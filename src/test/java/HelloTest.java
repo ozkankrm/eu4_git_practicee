4ebf15fbe2b2fe5cc0904090ee15914e178ef54c
ozkankrm <ozkankerim20@gmail.com>
2021-02-01 23:03:46 +0000

    Added new info

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello word test");
           System.out.println("Hello word test22222");
+          System.out.println("Hel");
 
     }
 }
